e202723ca62561d768b6630364a48fd3988ddd14
Gustavo Fonseca <gustavo.fonseca@cleversystems.com.br>
2023-02-07 16:57:12 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io.connect("http://127.0.0.1:3001");
+function App() {
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+  };
+
+  // const sendMessage = () => {
+  //   socket.emit("send_message", { message });
+  // };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
+  return (
+    <div className='App'>
+      <input
+        placeholder='Número da sala'
+        onChange={(event) => setRoom(event.target.value)}
+      />
+      <button onClick={joinRoom}>Entrar na sala</button>
+      <input
+        onChange={(event) => setMessage(event.target.value)}
+        placeholder='Mensagem...'
+      />
+      <button onClick={sendMessage}>Enviar Mensagem</button>
+      <h1>Mensagem:</h1>
+      <h3>{messageReceived}</h3>
+    </div>
+  );
+}
+
+export default App;
